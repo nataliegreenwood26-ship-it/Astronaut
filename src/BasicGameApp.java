11adfeacc985fc0cfa4caffb6f2be9e83636e7bc
nataliegreenwood <natalie_greenwood26@milton.edu>
2025-12-04 10:55:36 -0500

    Made random variables

--- src/BasicGameApp.java
+++ src/BasicGameApp.java
@@ -61,12 +61,30 @@ public class BasicGameApp implements Runnable {
 	public BasicGameApp() {
       
       setUpGraphics();
-       
+
+
+      //random structure
+        //(int)(Math.random() * range)
+        //range is 0-9
+        int randx = (int)(Math.random()*10) +1;
+
+        //range 1-999
+        randx = (int)(Math.random()*999) + 1;
+        //0.34-0.9999999
+        //0.1111-998.82
+        //1.111 - 999.87
+        //1-999
+
+        //todo: make variable randy that generated a random number between 1-699
+
+        int randy = (int)(Math.random()*699) + 1;
+
+
       //variable and objects
       //create (construct) the objects needed for the game and load up 
 		astroPic = Toolkit.getDefaultToolkit().getImage("astronaut.png"); //load the picture
 		astro = new Astronaut(WIDTH/2,HEIGHT/2);
-        astro2 = new Astronaut(400, 200);
+        astro2 = new Astronaut(randx, randy);
 
 
 	}// BasicGameApp()
